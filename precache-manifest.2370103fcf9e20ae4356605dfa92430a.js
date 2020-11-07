d636fce67826077d53fca5c88c595aa387f9a53e
PadNim14 <npadmana@purdue.edu>
2020-11-07 11:07:30 -0500

    Updates

---
+++ precache-manifest.2370103fcf9e20ae4356605dfa92430a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084389175c74dd52aaaec1761bf788de",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "aac9a9bf4a5b03da6a93",
+    "url": "/pokedex/static/css/main.a484416f.chunk.css"
+  },
+  {
+    "revision": "edbde1dd6420f5f63060",
+    "url": "/pokedex/static/js/2.0ee53f5e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pokedex/static/js/2.0ee53f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac9a9bf4a5b03da6a93",
+    "url": "/pokedex/static/js/main.0e083de6.chunk.js"
+  },
+  {
+    "revision": "ba342f49ac9f855b01df",
+    "url": "/pokedex/static/js/runtime-main.2981b98a.js"
+  },
+  {
+    "revision": "b120c11cb4013afab25036ed8eea9584",
+    "url": "/pokedex/static/media/sound_effect.b120c11c.mp3"
+  }
+]);
